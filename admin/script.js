8ea65d7195211cab6fe03ec9a3341f959c1aa284
Elias Ramos <19041239@itdurango.edu.mx>
2023-04-25 19:20:08 -0600

    FRONT: Panel de administrador agregado.

---
+++ admin/script.js
@@ -0,0 +1,6 @@
+const botonMostrarFormulario = document.querySelector("#mostrarFormulario");
+const formularioFlotante = document.querySelector("#formularioFlotante");
+
+botonMostrarFormulario.addEventListener("click", function() {
+  formularioFlotante.style.display = "block";
+});
